aff6fcb48b0c50ce2b7aa551de886c54a01a8a29
rtlortega <rtlortega@gmail.com>
2026-01-16 10:45:46 +0100

    [fix] error in file, same functionalities

--- workflow_matchms.py
+++ workflow_matchms.py
@@ -18,6 +18,8 @@ from matchms.similarity import ModifiedCosine, CosineGreedy, FlashSimilarity
 
 from phylo2MS.phylo2ms.networking.networking import build_base_graph, build_thresh_graph, build_core_rescue_graph
 
+import networkx as nx
+
 
 
 ### - Load and preprocess
@@ -57,11 +59,10 @@ df_mean_sim, df_edge_sup = calculate_boostrapping(
 df_mean_sim.to_csv("bootstrap_mean_similarity_Cos.csv")
 df_edge_sup.to_csv("bootstrap_edge_support_Cos.csv")
 
-import networkx as nx
-# Load ans maps
-gnps_network, id_map = load_gnps_graph_and_id_map("/Users/rtlortega/Documents/PhD/Phylo2MS/phylo2MS/experiments/runs/SpecReboot_workflow_-_GNPS-39397e9f1b7547a79931dd9f57ec2e80-network_singletons.graphml",
-                           df_mean_sim.index,
-                           candidate_node_attrs="shared name")
+    
+# Networking
+build_base_graph(df_mean_sim, df_edge_sup) #base similarity
+build_thresh_graph(df_mean_sim, df_edge_sup, sim_threshold = 0.7, support_threshold = 0.5) #two thresholds
 
-#Do the third recued network
-add_rescued_edges_to_gnps_graph(gnps_network, df_mean_sim, df_edge_sup)
+build_core_rescue_graph(df_mean_sim, df_edge_sup,sim_core = 0.7, support_core = 0.3, sim_rescue_min = 0.00001 ,support_rescue = 0.5, output_file="ms2dp_core_rescued_RIPPs.graphml")
+
